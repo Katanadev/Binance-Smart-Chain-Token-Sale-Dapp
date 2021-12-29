147471427a49e18e290fafe962752f00cdeda609
Baked <bakedcaketoken@gmail.com>
2021-12-29 12:12:26 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,321 @@
+import './App.css';
+import {useState, useEffect} from 'react'
+import {ADDRESS, ABI, NETWORK_ID} from './constants';
+import WalletConnect from "walletconnect";
+
+function App() {
+  
+  const $ = window.jQuery;
+
+  const [error, setError] = useState("");
+  const [errorA, setErrorA] = useState("");
+  const [errorAB, setErrorAB] = useState(false);
+  const [softcap, setSoftcap] = useState(0);
+  const [hardcap, setHardcap] = useState(0);
+  const [raised, setRaised] = useState(0);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
+  const [mine, setMine] = useState(0);
+  const [isWhitelisted, setIsWhiteListed] = useState(false);
+  const [onlyWhitelisted, setOnlyWhitelisted] = useState(false);
+  const [account, setAccount] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [contract, setContract] = useState("");
+  const [web3, setWeb3] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [started, setStarted] = useState(false);
+  const [completed, setCompleted] = useState(false);
+  const [id, setId] = useState('');
+  const [block, setBlock] = useState('');
+
+  const wc = new WalletConnect({
+    clientMeta: {description: 'My dApp'},
+  });
+
+
+  useEffect(() => {
+    if(localStorage.getItem('wc') === "true"){
+      setupWC();
+    }
+  } , [])
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setBlock(b=>!b);
+    }, 3000);
+    return () => {clearInterval(i)};
+  },[]);
+
+  useEffect(() => {
+    const  setup = async (_contract) => {
+      let hasStarted = await _contract.methods.openingTime().call();
+      let hasClosed = await _contract.methods.hasClosed().call()
+      let finalized = await _contract.methods
+        .isFinalized()
+        .call()
+
+      if (hasClosed || finalized) {
+        setCompleted(true);
+      }
+      if (hasStarted <= Date.now() / 1000) {
+        setStarted(true);
+      }
+    }
+    if(web3!==""){
+      const presale = new web3.eth.Contract(ABI, ADDRESS);
+      presale.methods.goal().call().then(setSoftcap);
+      presale.methods.cap().call().then(setHardcap);
+      presale.methods.weiRaised().call().then(setRaised);
+      presale.methods.minContribution().call().then(setMin);
+      presale.methods.maxContribution().call().then(setMax);
+      presale.methods.onlyWhitelisted().call().then(setOnlyWhitelisted);
+      setup(presale);
+      setContract(presale);
+
+      if(id === ''){
+        web3.eth.net.getId().then(_id => setId(_id));
+      }
+      web3.currentProvider.on("accountsChanged", function (accounts) {
+        setAccount(accounts[0]);
+      });
+
+      web3.currentProvider.on("networkChanged", function (networkId) {
+        web3.eth.net.getId().then((id) => setId(id));
+      });
+
+      web3.currentProvider.on("disconnect", function (error) {
+        localStorage.setItem("wc", false);
+        setConnected(false);
+        setWeb3("");
+      })
+    }
+  },[web3])
+
+  useEffect(()=>{
+    if(web3!==""){
+      const __amount = web3.utils.toWei(amount.toString(), "ether");
+      if (+__amount > +max) {
+        setErrorA("Amount cannot be greater than max");
+        setErrorAB(true);
+      } else if (+__amount < +min) {
+        setErrorA("Amount cannot be less than min");
+        setErrorAB(true);
+      } else {
+        setErrorA("");
+        setErrorAB(false);
+      }
+    }
+  },[web3, amount, min, max])
+
+  const handleChange = (e)=>{
+    setAmount(+e.target.value);
+  }
+
+  useEffect(() => {
+    const fetch = async () => {
+      if(!web3){
+        if (window.ethereum || window.web3) {
+          const web3 = new window.Web3(window.web3.currentProvider); 
+          setWeb3(web3);
+        }else{
+          setError("Please install MetaMask or TrustWallet");
+        }
+      }else{
+        let accounts = [];
+        try {
+          accounts = await web3.currentProvider.enable();
+          const _account = web3.utils.toChecksumAddress(accounts[0]);
+          if(_account !== account){
+            setAccount(_account);
+            setConnected(true);
+          }
+        } catch (e) {
+          setConnected(false);
+        }
+        if (id !== NETWORK_ID && id !== '') {
+          setLoading(false);
+          setError(
+            `Wrong network, Switch to Smart Chain ${NETWORK_ID === 97 && "Testnet"}`
+          );
+          return;
+        }
+        if (connected) {
+          const _mine = await contract.methods.getUserContribution(account).call();
+          const _isWhitelisted = await contract.methods.whitelist(account).call();
+
+          setMine(_mine);
+          setIsWhiteListed(_isWhitelisted);
+          
+          
+          setError(
+            `You've bought ${web3.utils.fromWei(
+              _mine.toString(),
+              "ether"
+            )} out of ${web3.utils.fromWei(max.toString(), "ether")} BNB`
+          );
+        }
+
+        if(loading){
+          setLoading(false);
+        }
+      }
+    }
+    fetch();
+  }, [account, $, web3, id, block]);
+
+  const connect = async (e) => {
+    try {
+      const accounts = await web3.currentProvider.enable();
+      setAccount(accounts[0]);
+      setConnected(true);
+    } catch (e) {
+      setConnected(false);
+    }
+  }
+
+  const setupWC = async () =>{
+    try{
+      await wc.connect({
+        chainId: NETWORK_ID,
+        rpcUrl: "https://bsc-dataseed.binance.org",
+      });
+      const provider = wc.getWeb3Provider({
+        rpc: {56:"https://bsc-dataseed.binance.org"},
+        rpcUrl: "https://bsc-dataseed.binance.org",
+      });
+      localStorage.setItem("wc", true);
+      setWeb3(new window.Web3(provider));
+      setConnected(true);
+    }
+    catch(e){}
+  }
+
+  const buy = async (e) => {
+    e.preventDefault();
+    $(e.target).text("Buying...");
+    $(e.target).attr("disabled", true);
+    try{
+      await contract.methods
+      .buyTokens(account)
+      .send({ from: account, value: web3.utils.toWei(amount.toString(), "ether") });
+    }catch(e){
+      $(e.target).text("Buy Tokens");
+      $(e.target).removeAttr("disabled");      
+    }
+    $(e.target).text("Buy Tokens");
+    $(e.target).removeAttr("disabled");
+  }
+
+  return (
+    <div className="App">
+      <header
+        className="App-header"
+        style={{ backgroundImage: "url(/bg-min.jpeg)" }}
+      >
+        {web3 ? (
+          loading ? (
+            <p>LOADING...</p>
+          ) : connected ? (
+            error.startsWith("Wrong network") ? (
+              <p>{error}</p>
+            ) : started ? (
+              !completed ? (
+                <div className="rounded bg-dark p-5 opacity">
+                  <div>
+                    TOTAL RAISED: <span className="pink-text">{web3.utils.fromWei(raised.toString(), "ether")}</span> BNB
+                    <br />
+                    SOFT CAP: <span className="pink-text">{web3.utils.fromWei(softcap.toString(), "ether")}</span> BNB
+                    <br />
+                    HARD CAP: <span className="pink-text">{web3.utils.fromWei(hardcap.toString(), "ether")}</span> BNB
+                    <br />
+                    MIN. BUY: <span className="pink-text">{web3.utils.fromWei(min.toString(), "ether")}</span> BNB
+                    <br />
+                    MAX. BUY: <span className="pink-text">{web3.utils.fromWei(max.toString(), "ether")}</span> BNB
+                    <br />
+                    MY CONTRIBUTION: <span className="pink-text">{web3.utils.fromWei(mine.toString(), "ether")}</span> BNB
+                    <p>{onlyWhitelisted && isWhitelisted ? error : ""}</p>
+                    <p>{onlyWhitelisted && isWhitelisted ? errorA : ""}</p>
+                  </div>
+                  {!onlyWhitelisted || (onlyWhitelisted && isWhitelisted) ? (
+                    <>
+                      <input
+                        type="number"
+                        placeholder="Enter amount"
+                        onChange={handleChange}
+                        className="w-100 py-2 px-3 text-center"
+                      />
+                      <p className="my-3 text-center">1 BNB = 100,000,000 CANYON</p>
+                      <div className="text-center my-2">
+                        <button className="btn btn-main"
+                          onClick={buy}
+                          style={{
+                            display:
+                              web3.utils.fromWei(mine.toString(), "ether") >=
+                                web3.utils.fromWei(max.toString(), "ether") ||
+                              errorAB
+                                ? "none"
+                                : "block"
+                          }}
+                        >
+                          Buy Tokens
+                        </button>
+                      </div>
+                      <div className="small-text mt-4"
+                           style={{
+                             display:
+                                 web3.utils.fromWei(mine.toString(), "ether") >=
+                                 web3.utils.fromWei(max.toString(), "ether") ||
+                                 errorAB
+                                     ? "none"
+                                     : "block"
+                           }}
+                      >
+                        <p className="p-0 mt-0 mx-0 mb-3 fw-bold">
+                          Don't forget to add $CANYON as as a custom token to your wallet!
+                        </p>
+                        <p className="p-0 m-0">
+                          Contract: <span className="pink-text">0xF08431B7A3bcF69f375fa4c230425150473A3Fb8</span>
+                        </p>
+                        <p className="p-0 m-0">
+                          Name: <span className="pink-text">CODECANYON</span>
+                        </p>
+                        <p className="p-0 m-0">
+                          Symbol: <span className="pink-text">CANYON</span>
+                        </p>
+                        <p className="p-0 m-0">
+                          Decimals: <span className="pink-text">18</span>
+                        </p>
+                      </div>
+                    </>
+                  ) : (
+                    <p>YOU ARE NOT WHITELISTED, WAIT FOR PUBLIC LISTING</p>
+                  )}
+                </div>
+              ) : (
+                <p>PRIVATE SALE HAS ALREADY CLOSED</p>
+              )
+            ) : (
+              <p>PRIVATE SALE HAS NOT STARTED YET</p>
+            )
+          ) : (
+            <>
+              <p>{"USE METAMASK OR TRUSTWALLET"}</p>
+              <button className="btn btn-main" onClick={connect}>
+                CONNECT
+              </button>
+            </>
+          )
+        ) : (
+          <>
+            <p>INSTALL METAMASK / TRUSTWALLET</p>
+            <p>OR</p>
+            <button className="btn btn-main" onClick={setupWC}>Wallet Connect</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,472 @@
+export const ADDRESS = "0xF08431B7A3bcF69f375fa4c230425150473A3Fb8";
+export const ABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [],
+    name: "Finalized",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "purchaser",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "beneficiary",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "TokenPurchase",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "_beneficiaries",
+        type: "address[]",
+      },
+    ],
+    name: "addManyToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_beneficiary",
+        type: "address",
+      },
+    ],
+    name: "addToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_beneficiary",
+        type: "address",
+      },
+    ],
+    name: "buyTokens",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cap",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "capReached",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "claimRefund",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "closingTime",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "contributions",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "finalize",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_beneficiary",
+        type: "address",
+      },
+    ],
+    name: "getUserContribution",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "goal",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "goalReached",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "hasClosed",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "isFinalized",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxContribution",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "minContribution",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "onlyWhitelisted",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "openingTime",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "rate",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "reclaimToken",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_beneficiary",
+        type: "address",
+      },
+    ],
+    name: "removeFromWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "_beneficiaries",
+        type: "address[]",
+      },
+    ],
+    name: "removeManyFromWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "takeBack",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "toggleWhitelisting",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "token",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "vault",
+    outputs: [
+      {
+        internalType: "contract RefundVault",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "wallet",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "weiRaised",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelist",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+export const NETWORK_ID = 56;
+
+// const t_address = "0xEBfeaDD37C5b0db20140Fe515740d0669529055E";
+// const t_address = "0xb96c7C6C233A57B3A0ee98cCB1500973B358e4CD";
+// const t_address = "0x927Acb7E34A112796C5c65590e20087c0d4fD94F";
+// const t_address = "0xd6B689e7bC378454Eaeb53d44aD5AEd991b6Db34";
